96d0ae23b9c31ec2db589a9cadbe15b0ea42ff24
LeeConroy1977 <leejconroy@gmail.com>
2025-01-20 14:05:24 +0000

    netlify added

--- functions/server.js
+++ functions/server.js
@@ -1,45 +1,12 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // Your JSON file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-  if (req.method === "GET" && req.url.startsWith("/groups/")) {
-    const groupId = req.url.split("/").pop();
-    const group = router.db.get("groups").find({ id: groupId }).value();
 
-    if (group && !group.approved) {
-      return res
-        .status(403)
-        .json({ error: "Access denied. Group is not approved." });
-    }
-  }
-  next();
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
-
-server.use((req, res, next) => {
-  if (req.method === "GET" && req.url.startsWith("/events/")) {
-    const eventId = req.url.split("/").pop();
-    const event = router.db.get("event").find({ id: eventId }).value();
-
-    if (event && !event.approved) {
-      return res
-        .status(403)
-        .json({ error: "Access denied. event is not approved." });
-    }
-  }
-  next();
-});
-
-exports.handler = async (event, context) => {
-  const port = 3000;
-  server.listen(port, () => {
-    console.log(`JSON Server running at http://localhost:${port}`);
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "JSON Server is running" }),
-  };
-};
